2d660c1686a253b2c9be86f0b65adff3a8287397
0xbhavesh404dev <kbhavesh2007@gmail.com>
2026-02-15 19:02:52 +0530

    _1

---
+++ Projectreview1/src/Task.java
@@ -0,0 +1,46 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+    private static int count=0;
+    private final int id;
+    private String task_name;
+    private LocalDateTime deadline;
+    private int priority;
+    private int status;
+
+    public Task(String task_name, String deadline, int priority) {
+        this.task_name = task_name.trim().toLowerCase();
+        this.deadline = LocalDateTime.parse(deadline, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.priority = priority;
+        this.status = 0;
+        this.id = ++count;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getTaskName() {
+        return task_name;
+    }
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void markCompleted() {
+        status = 1;
+    }
+
+    public void markPending() {
+        status = 0;
+    }
+    public int getPriority() {
+        return priority;
+    }
+
+
+}
